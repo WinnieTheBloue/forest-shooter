99ca291ba84fec721c899b8c78d5c731680eb755
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-03-04 12:08:41 +0100

    Mesh size

--- src/components/TheMesh.vue
+++ src/components/TheMesh.vue
@@ -1,6 +1,6 @@
 <template>
   <a-entity
-    geometry="primitive: plane; height: 1; width: 1"
+    geometry="primitive: plane; height: 0.1; width: 0.1"
     position="0 0.01 0"
     rotation="-90 0 0"
     data-role="nav-mesh"
